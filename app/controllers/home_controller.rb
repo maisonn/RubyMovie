f6e248e56d5749aa11b7adf181ce6a84a0c4a934
maisonn <37782167+maisonn@users.noreply.github.com>
2021-05-25 17:55:09 +0300

    1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @movies = Movie.all.order(created_at: :desc)
+    if params.has_key?(:category)
+      @category = Category.find_by_name(params[:category])
+      @movies = Movie.where(category: @category)
+    else
+      @movies = Movie.all.order(created_at: :desc)
+    end
   end
 end
