6011f31e0b2372f083b336648df153023494bbb0
maisonn <37782167+maisonn@users.noreply.github.com>
2021-05-25 20:36:57 +0300

    Ratings Working!

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
     belongs_to :category # belong to one category
+    has_many :reviews, dependent: :destroy
     has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   resources :categories
-  resources :movies
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
